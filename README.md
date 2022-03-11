55d317537b1a37065a35e63e108db305a407fa1c
Kevin Lamping <klamping@gmail.com>
2022-03-11 15:57:33 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# README
+
+To run the tests via Docker:
+
+1. Ensure Docker is up and running
+2. Run `npm run test:docker`
---
+++ test/specs/sample.js
@@ -0,0 +1,5 @@
+describe('My Test', function () {
+    it('should work', async function () {
+        await browser.url('https://demo.learnwebdriverio.com')
+    })
+})
